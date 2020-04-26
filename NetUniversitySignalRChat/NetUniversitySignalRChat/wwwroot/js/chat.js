00aa2888fb886280d24f92abed46969739e3af36
mbotero <mbotero@jwprojecthouse.com>
2020-04-26 01:13:59 -0500

    Add signal library and js to running chat

--- NetUniversitySignalRChat/NetUniversitySignalRChat/wwwroot/js/chat.js
+++ NetUniversitySignalRChat/NetUniversitySignalRChat/wwwroot/js/chat.js
@@ -5,7 +5,7 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("RecibirMensaje", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var encodedMsg = user + " says " + msg;
     var li = document.createElement("li");
@@ -20,9 +20,9 @@ connection.start().then(function () {
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    var usuario = document.getElementById("userInput").value;
+    var mensaje = document.getElementById("messageInput").value;
+    connection.invoke("EnviarMensaje", usuario, mensaje).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
